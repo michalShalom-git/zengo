393ea2b3a64e1f2da6b931f5580cbbf3b622088f
michalShalom-git <michal159052@gmail.com>
2022-05-18 16:33:17 +0300

    Create function to test the browser launching

--- src/zengo/Zengo.java
+++ src/zengo/Zengo.java
@@ -2,23 +2,36 @@ package zengo;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+import org.junit.Assert;
 
 public class Zengo {
 	WebDriver driver;
-	
+
 	public Zengo() {
-		System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromeDriver.exe");
+		String path = System.setProperty("webdriver.chrome.driver", "C:\\chromeDriver\\chromeDriver.exe");
+		System.out.print(path);
 		driver = new ChromeDriver();
 	}
 
-	public void launchBrowser() {
+	public void launchBrowser() throws InterruptedException {
 		driver.get("https://zengo.com/");
 	}
 
-	public static void main(String[] args) {
+	@Test
+	public void testAssertLaunching() throws InterruptedException {
+		String title = driver.getTitle();
+		String ExpectedTitle = "ZenGo - Simple & Secure Crypto Wallet App";
+		Assert.assertEquals(ExpectedTitle, title);
+	}
+	
+		public static void main(String[] args) throws InterruptedException {
 		Zengo z1 = new Zengo();
 		z1.launchBrowser();
+		z1.testAssertLaunching();
 	}
 
 
